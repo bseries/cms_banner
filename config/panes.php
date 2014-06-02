1301a53ea7a9f03097f39191ce76dabce0609f80
David Persson <davidpersson@gmx.de>
2014-06-02 23:30:50 +0200

    adding missing files.

---
+++ config/panes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Bureau Banner
+ *
+ * Copyright (c) 2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+use cms_core\extensions\cms\Panes;
+use lithium\g11n\Message;
+
+extract(Message::aliases());
+
+$base = ['controller' => 'banners', 'library' => 'cms_banner', 'admin' => true];
+Panes::registerActions('cms_banner', 'authoring', [
+	$t('List Banners') => ['action' => 'index'] + $base,
+	$t('New Banner') => ['action' => 'add'] + $base
+]);
+
+?>
---
+++ config/widgets.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Bureau Media
+ *
+ * Copyright (c) 2013-2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+use lithium\g11n\Message;
+use cms_core\extensions\cms\Widgets;
+use cms_banner\models\Banners;
+
+extract(Message::aliases());
+
+Widgets::register('cms_banner', 'authoring',  function() use ($t) {
+	return [
+		'data' => [
+			$t('Banners') => Banners::find('count')
+		]
+	];
+}, [
+	'type' => Widgets::TYPE_TABLE,
+	'group' => Widgets::GROUP_DASHBOARD
+]);
+
+?>
